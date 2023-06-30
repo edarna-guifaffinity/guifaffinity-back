c9a94ad777ca1d745d53def8ddd1a842ff30a148
Nacho de M <nachodmv@outlook.es>
2023-06-30 19:06:14 +0200

    test return gif by id

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,6 +5,7 @@ import Lowdb from "lowdb";
 import Memory from "lowdb/adapters/Memory";
 import { DatabaseSchema } from "./DatabaseSchema";
 import dbData from "./fixtures/db.json";
+import { Gif } from "model/gif.model";
 
 describe("/api/memes", () => {
   let app: Express;
@@ -38,6 +39,27 @@ describe("/api/memes", () => {
       .expect(200)
       .then((response) => {
         expect(response.body).toHaveLength(50);
+        done();
+      });
+  });
+
+  it("return a gif", (done) => {
+    const gif: Gif = {
+      id: "YleuWir5NTNVXkflSp",
+      src: "https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif",
+      title: "Movie Brazil GIF by MOODMAN",
+      tags: ["#movie", "#brazil", "#brazil the movie"],
+      user: {
+        avatar: "",
+        name: "",
+      },
+    };
+
+    request(app)
+      .get("api/gifs/YleuWir5NTNVXkflSp")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(gif);
         done();
       });
   });
