2df4aea5b9dd3c9c2622a93a546291617e11fa67
Nacho de M <nachodmv@outlook.es>
2023-06-30 18:26:50 +0200

    Interfaz Meme

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Request, Response, Router } from "express";
+import { Gif } from "model/gif.model";
 
 export const routes = Router();
 
 routes.get("/memes", (req: Request, res: Response) => {
   const db = req.context.db;
   const memes = db.get("memes").take(50).value();
+  memes.forEach((meme) => {
+    console.log(meme?.id);
+  });
 
   res.status(200).json(memes);
 });
